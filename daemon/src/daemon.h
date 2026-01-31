6249577374ffd8fd9edd5e21f28515975d2132ed
Udbhav-Dubey <udbhavdubey44@gmail.com>
2026-01-31 18:47:41 +0530

    basic daemon done

--- daemon/src/daemon.h
+++ daemon/src/daemon.h
@@ -1,2 +1,25 @@
 #pragma once
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+class Daemon{
+    private:
+        bool running;
+        bool busy;
+        void loopy();
+        std::mutex mtx;
+        std::condition_variable cv;
+        std::thread worker;
+        //std::queue<>work_queue;
+    public:
+        Daemon();
+        void start();
+        void stop();
+        bool is_running()const;
+        bool is_busy()const;
+        void work();
+        Daemon(const Daemon&)=delete;
+        Daemon&operator=(const Daemon&)=delete;
+        ~Daemon();
+};
 
